c4d0983c93c864cadb605ad8e6ed2b065a834f62
jenish-codes <jenish.codes@gmail.com>
2022-07-26 15:54:52 +0530

    Contact model created

--- food_shop/models.py
+++ food_shop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    added_on = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Contact Table"
