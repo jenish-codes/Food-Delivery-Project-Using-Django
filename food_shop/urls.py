119a1473cdab3ca0ba4650f9b2271eaec7e0b232
jenish-codes <jenish.codes@gmail.com>
2022-07-26 15:30:58 +0530

    all templates configured succesfully

--- food_shop/urls.py
+++ food_shop/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import home
+from .views import home, contact, about, feature, menu, chef, blog, blogDetails, booking
 
 
 urlpatterns = [
     path('', home, name="home"),
+    path('about/', about, name="about"),
+    path('feature/', feature, name="feature"),
+    path('chef/', chef, name="chef"),
+    path('menu/', menu, name="menu"),
+    path('booking/', booking, name="booking"),
+    path('blog/', blog, name="blog"),
+    path('blog-details/', blogDetails, name="blog-details"),
+    path('contact-us/', contact, name="contact-us"),
 ]
--- food_shop/views.py
+++ food_shop/views.py
@@ -5,3 +5,27 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "food_shop/index.html")
+
+def about(request):
+    return render(request, "food_shop/about.html")
+
+def feature(request):
+    return render(request, "food_shop/feature.html")
+
+def chef(request):
+    return render(request, "food_shop/team.html")
+
+def menu(request):
+    return render(request, "food_shop/menu.html")
+
+def booking(request):
+    return render(request, "food_shop/booking.html")
+
+def blog(request):
+    return render(request, "food_shop/blog.html")
+
+def blogDetails(request):
+    return render(request, "food_shop/single.html")
+
+def contact(request):
+    return render(request, "food_shop/contact.html")
